c8cc04e849f7627de632362fc34d2002b561006b
petertechdev <peterleite.dev@gmail.com>
2022-10-28 04:51:51 -0300

    Create EmailService

---
+++ src/main/java/com/petertech/emailSender/controllers/EmailController.java
@@ -0,0 +1,30 @@
+package com.petertech.emailSender.controllers;
+
+import com.petertech.emailSender.dtos.EmailDto;
+import com.petertech.emailSender.models.EmailModel;
+import com.petertech.emailSender.services.EmailService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+
+}
--- src/main/java/com/petertech/emailSender/services/EmailService.java
+++ src/main/java/com/petertech/emailSender/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.petertech.emailSender.services;
 
+import com.petertech.emailSender.models.EmailModel;
 import com.petertech.emailSender.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class EmailService {
 
     @Autowired
     EmailRepository emailRepository;
+
+    public void sendEmail(EmailModel emailModel) {
+
+    }
 }
